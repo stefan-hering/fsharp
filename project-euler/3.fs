10c7bd91271f8709ffee311bb8d78a9b545b8b57
stefan-hering <stefanvhering@gmail.com>
2017-10-08 16:41:35 +0200

    Optimized Euler 3

--- project-euler/3.fs
+++ project-euler/3.fs
@@ -1,11 +1,12 @@
 
 let n = 600851475143L
-let rec sieve i max numbers =
-    // Take out all multiples of i from the list
-    let filteredNumbers = List.filter (fun n -> n <= i || n % i <> 0) numbers
-    if i = max then filteredNumbers
-    else sieve (i+1) max filteredNumbers
+let rec sieve i numbers =
+    if i = List.length numbers then numbers
+    else
+        let p = List.nth numbers i
+        // Take out all multiples of i from the list and continue
+        sieve (i+1) (List.filter (fun n -> n <= p || n % p <> 0) numbers)
 
-let primes = sieve 2 10000 [2..10000]
+let primes = sieve 0 [2..100000]
 
 let result = List.max (List.filter (fun p -> n % (int64 p) = 0L) primes)
